4ac4e8a6027a691d95753432f2fd21a194ffd88c
Djellzahel <djellza.helshani.9@gmail.com>
2022-06-26 17:29:22 +0200

    changes

--- futboll/src/App.js
+++ futboll/src/App.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import logo from './logo.svg'; 
 import './App.css';
 import { Link } from 'react-router-dom'
--- futboll/src/Footer.js
+++ futboll/src/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-<footer className="footer">
+    <footer className="footer">
       <div className="container1">
         <div className="row">
           <div className="footer-col">
